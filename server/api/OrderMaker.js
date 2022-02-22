e4e2cf1cfa254fa41526217cb33b4b21a32b21ea
Arthur Plebs <arthur.plebs@outlook.fr>
2022-02-22 23:23:56 +0100

    wip

--- server/api/OrderMaker.js
+++ server/api/OrderMaker.js
@@ -33,7 +33,11 @@ class OrderMaker {
     }
 
     async limitBuy(strategyId, quantity, price) {
-        let order = await this.clients[strategyId].broker.futuresBuy(this.currency, quantity, price);
+        let order = await this.clients[strategyId].broker.futuresBuy(
+            this.clients[strategyId].currency,
+            quantity,
+            price
+        );
         this.emitNewOrder({
             strategyId: strategyId,
             type: "limit",
@@ -45,53 +49,125 @@ class OrderMaker {
     }
 
     async limitSell(strategyId, quantity, price) {
-        let order = await this.clients[strategyId].broker.futuresSell(this.currency, quantity, price);
+        let order = await this.clients[strategyId].broker.futuresSell(
+            this.clients[strategyId].currency,
+            quantity,
+            price
+        );
+        this.emitNewOrder({
+            strategyId: strategyId,
+            type: "limit",
+            side: "sell",
+            price: price,
+            quantity : quantity,
+        })
+        return await this.isFullyExecuted(order, strategyId);
     }
 
     async marketBuy(strategyId, quantity) {
-        let order = await this.clients[strategyId].broker.futuresMarketBuy(this.currency, quantity);
+        let order = await this.clients[strategyId].broker.futuresMarketBuy(
+            this.clients[strategyId].currency,
+            quantity,
+            { newOrderRespType: 'RESULT' }
+        );
+        this.emitNewOrder({
+            strategyId: strategyId,
+            type: "market",
+            side: "buy",
+            price: order.avgPrice,
+            quantity : quantity,
+        })
+        return true;
     }
 
     async marketSell(strategyId, quantity) {
-        let order = await this.clients[strategyId].broker.futuresMarketSell()(this.currency, quantity);
+        let order = await this.clients[strategyId].broker.futuresMarketSell()(
+            this.clients[strategyId].currency,
+            quantity,
+            { newOrderRespType: 'RESULT' }
+        );
+        this.emitNewOrder({
+            strategyId: strategyId,
+            type: "market",
+            side: "sell",
+            price: order.avgPrice,
+            quantity : quantity,
+        })
+        return true;
     }
 
     async setShortStopLoss(strategyId, quantity, stopPrice) {
-        let order = await this.clients[strategyId].broker.futuresMarketBuy(this.currency, quantity, {stopPrice : stopPrice, type: "STOP_MARKET"});
+        let order = await this.clients[strategyId].broker.futuresMarketBuy(
+            this.clients[strategyId].currency,
+            quantity,
+            {stopPrice : stopPrice, type: "STOP_MARKET"} //revoir le type
+        );
+        this.emitNewOrder({
+            strategyId: strategyId,
+            type: "stop_market",
+            side: "buy",
+            stopPrice: stopPrice,
+            quantity : quantity,
+        })
+        return order.orderId;
     }
 
     async setLongStopLoss(strategyId, quantity, stopPrice) {
-        let order = await this.clients[strategyId].broker.futuresMarketSell(this.currency, quantity, {stopPrice : stopPrice, type: "STOP_MARKET"});
+        let order = await this.clients[strategyId].broker.futuresMarketSell(
+            this.clients[strategyId].currency,
+            quantity,
+            {stopPrice : stopPrice, type: "STOP_MARKET"} // revoir le type
+        );
+        this.emitNewOrder({
+            strategyId: strategyId,
+            type: "stop_market",
+            side: "sell",
+            stopPrice: stopPrice,
+            quantity : quantity,
+        })
+        return order.orderId;
     }
 
     async setLongTakeProfit(strategyId, quantity, price, type) {
-        let order = await this.clients[strategyId].broker.futuresSell(this.currency, quantity, {stopPrice : price, type: type});
+        let order = await this.clients[strategyId].broker.futuresSell(
+            this.clients[strategyId].currency,
+            quantity,
+            {stopPrice : price, type: type} // faire des constantes pour les types
+        );
+        this.emitNewOrder({
+            strategyId: strategyId,
+            type: "stop_market",
+            side: "sell",
+            stopPrice: stopPrice,
+            quantity : quantity,
+        })
+        return order.orderId;
     }
 
     async setShortTakeProfit(strategyId, quantity, price, type) {
-        let order = await this.clients[strategyId].broker.futuresBuy(this.currency, quantity, {stopPrice : price, type: type});
+        let order = await this.clients[strategyId].broker.futuresBuy(
+            this.clients[strategyId].currency,
+            quantity, {stopPrice : price, type: type}
+        );
+        //TODO
     }
 
     async getPositions(strategyId) {
         let positions = await this.clients[strategyId].broker.futuresPositionRisk()
+        //TODO
     }
 
     async setLeverage(strategyId, leverage) {
-        let lever = await this.clients[strategyId].broker.futuresLeverage( this.currency, leverage)
+        let lever = await this.clients[strategyId].broker.futuresLeverage(
+            this.clients[strategyId].currency,
+            leverage
+        )
+        //TODO
     }
 
     async cancelAllOrders(strategy) {
-        let res = await this.clients[strategyId].broker.cancelAll(this.currency);
-    }
-
-    isSuccess(response) {
-        if (response.code !== undefined) {
-            logger.error(response);
-            //stoper la strat
-            return false;
-        }
-
-        return true;
+        let res = await this.clients[strategyId].broker.cancelAll(this.clients[strategyId].currency);
+        //TODO
     }
 
     async getOrderStatus(orderId) {
@@ -102,8 +178,8 @@ class OrderMaker {
     async isFullyExecuted(response, strategyId) {
         let fullyExecuted = new Promise( (resolve, reject) => {
             setTimeout( async () => {
-                let orderStatus = await this.getOrderStatus(response.OrderId)
-                resolve(orderStatus)
+                    let orderStatus = await this.getOrderStatus(response.OrderId)
+                    resolve(orderStatus)
                 },
                 limitOrderTimeout
             )
@@ -120,12 +196,13 @@ class OrderMaker {
     }
 
     emitNewOrder({
-        strategyId,
-        type,
-        side,
-        price,
-        quantity,
-        stopPrice,})
+                     strategyId,
+                     type,
+                     side,
+                     price,
+                     quantity,
+                     stopPrice,}
+    )
     {
         let newOrder = Order.build({
             strategyId: strategyId,
